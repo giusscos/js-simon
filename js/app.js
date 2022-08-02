2f5d1480ffee3f1e8018cd3694b48752fe01b987
giusscos <giu.cosenza08@gmail.com>
2022-08-02 16:06:22 +0000

    Add JS

---
+++ js/app.js
@@ -0,0 +1,43 @@
+function genIntNum(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function genSimonArray(){
+    const simonArray = []
+    for(let i=0; i < 5; i++){
+        simonArray.push(genIntNum(1, 100));
+    }
+    return simonArray
+}
+
+function getNumPlayer(){
+    const playerArray = []
+    for(let i = 0; i < 5; i++){
+        playerArray.push(parseInt(prompt('Inserisci il numero >> ' + (i+1) + ';')));
+    }
+    return playerArray
+}
+
+function matchSimonNum(numSimon, numPlayer){
+    if(numSimon === numPlayer){
+        console.log('Hai preso il >> ', numPlayer);
+        return numPlayer;
+    }
+    console.log('Hai dimenticato il >> ', numSimon);
+    return numSimon;
+}
+
+const simonArray = genSimonArray();
+alert(simonArray);
+
+const playerArray = getNumPlayer();
+setTimeout(playerArray, 5000);
+
+for(let i = 0; i < 5; i++){
+    matchSimonNum(simonArray[i], playerArray[i]);
+}
+
+
+// console.log('Simon >> ', genSimonArray(), ' || Player >> ', getNumPlayer(), ';')
